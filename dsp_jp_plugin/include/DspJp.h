ead6de7a40f31f61d9f51c791c1cd21f43a8be3d
skyshaver <skyshaver@me.com>
2026-06-23 12:28:24 -0700

    formatting and namespace additions

--- dsp_jp_plugin/include/DspJp.h
+++ dsp_jp_plugin/include/DspJp.h
@@ -1,36 +1,36 @@
 #pragma once
 
 namespace dsp_jp {
-class DspJp {
-public:
-  void prepare(double sampleRate, int expectedMaxFramesPerBlock) {
-    juce::ignoreUnused(sampleRate, expectedMaxFramesPerBlock);
-  }
-
-  void setParameterOne(float pOne) {
-      parameterOne = pOne;
-  }
-
-  void process(juce::AudioBuffer<float>& buffer) noexcept {
-  
-      // for each frame
-    for (const auto frameIndex : std::views::iota(0, buffer.getNumSamples())) {      
-      // for each channel sample in the frame
-      for (const auto channelIndex :
-           std::views::iota(0, buffer.getNumChannels())) {      
-        const auto inputSample = buffer.getSample(channelIndex, frameIndex);      
-        const auto outputSample = inputSample * parameterOne;      
-        buffer.setSample(channelIndex, frameIndex, outputSample);
+    class DspJp {
+    public:
+      void prepare(double sampleRate, int expectedMaxFramesPerBlock) {
+        juce::ignoreUnused(sampleRate, expectedMaxFramesPerBlock);
       }
-    }
-  }
 
-  void reset() noexcept {}
+      void setParameterOne(float pOne) {
+          parameterOne = pOne;
+      }
 
-private:
-    float parameterOne{ 0.01f };
+      void process(juce::AudioBuffer<float>& buffer) noexcept {
   
-};
+          // for each frame
+        for (const auto frameIndex : std::views::iota(0, buffer.getNumSamples())) {      
+          // for each channel sample in the frame
+          for (const auto channelIndex :
+               std::views::iota(0, buffer.getNumChannels())) {      
+            const auto inputSample = buffer.getSample(channelIndex, frameIndex);      
+            const auto outputSample = inputSample * parameterOne;      
+            buffer.setSample(channelIndex, frameIndex, outputSample);
+          }
+        }
+      }
+
+      void reset() noexcept {}
+
+    private:
+        float parameterOne{ 0.01f };
+  
+    };
 }  // namespace dsp_jp
 
 
--- dsp_jp_plugin/include/Phaser.h
+++ dsp_jp_plugin/include/Phaser.h
@@ -22,101 +22,106 @@
 
 */
 
-#define SAMPLE_RATE (44100.f)  //sample rate
-#define PI_FLOAT (3.14159f)
-
-class Phaser {
-public:
-    Phaser()  //initialise to some useful defaults...
-        : feedBack(.7f)
-        , lfoPhase(0.f)
-        , depth(1.f)
-        , zm1(0.f)
-    {
-        Range(440.f, 1600.f);
-        Rate(.5f);
-    }
-
-    void Range(float fMin, float fMax) { // Hz
-        dmin = fMin / (SAMPLE_RATE / 2.f);
-        dmax = fMax / (SAMPLE_RATE / 2.f);
-    }
-
-    void Rate(float rate) { // cps
-        lfoInc = 2.f * PI_FLOAT * (rate / SAMPLE_RATE);
-    }
-
-    void Feedback(float fb) { // 0 -> < 1.
-        feedBack = fb;
-    }
-
-    void Depth(float d) {  // 0 -> 1.
-        depth = d;
-    }
-
-    float Update(float inSamp) {
-        //calculate and update phaser sweep lfo...
-        float d = dmin + (dmax - dmin) * ((sin(lfoPhase) + 1.f) / 2.f);
-        lfoPhase += lfoInc;
-        if (lfoPhase >= PI_FLOAT * 2.f) {
-            lfoPhase -= PI_FLOAT * 2.f;
-        }
-
-        //update filter coeffs
-        //for (int i = 0; i < 6; i++) {
-        //    allPassDelays[i].Delay(d);
-        //}
-
-        for (auto& e : allPassDelays) {
-            e.Delay(d);
-        }
-
-        //calculate output
-        //float y = allPassDelays[0].Update(
-        //    allPassDelays[1].Update(
-        //        allPassDelays[2].Update(
-        //            allPassDelays[3].Update(
-        //                allPassDelays[4].Update(
-        //                    allPassDelays[5].Update(inSamp + zm1 * feedBack))))));
-
-        // updated for readibility, needs testing see https://godbolt.org/z/P54ve5o51 for live test
-        auto y = std::accumulate(std::next(allPassDelays.rbegin()), allPassDelays.rend(),
-            allPassDelays.back().Update(inSamp + zm1 * feedBack), [](auto acc, auto& e) { return e.Update(acc); });
-        zm1 = y;
-
-        return inSamp + y * depth;
-    }
-private:
-    class AllPassDelay {
+namespace dsp_jp {
+    class Phaser {
     public:
-        AllPassDelay()
-            : a1(0.f)
+        Phaser()  //initialise to some useful defaults...
+            : feedBack(.7f)
+            , lfoPhase(0.f)
+            , depth(1.f)
             , zm1(0.f)
         {
+            Range(440.f, 1600.f);
+            Rate(.5f);
         }
 
-        void Delay(float delay) { // sample delay time
-            a1 = (1.f - delay) / (1.f + delay);
+        void Prepare(double sr, int expectedMaxFramesPerBlock) {
+            sampleRate = sr;
+        }
+
+        void Range(float fMin, float fMax) { // Hz
+            dmin = fMin / (sampleRate / 2.f);
+            dmax = fMax / (sampleRate / 2.f);
+        }
+
+        void Rate(float rate) { // cps
+            lfoInc = 2.f * juce::MathConstants<float>::pi * (rate / sampleRate); 
+        }
+
+        void Feedback(float fb) { // 0 -> < 1.
+            feedBack = fb;
+        }
+
+        void Depth(float d) {  // 0 -> 1.
+            depth = d;
         }
 
         float Update(float inSamp) {
-            float y = inSamp * -a1 + zm1;
-            zm1 = y * a1 + inSamp;
+            //calculate and update phaser sweep lfo...
+            float d = dmin + (dmax - dmin) * ((sin(lfoPhase) + 1.f) / 2.f);
+            lfoPhase += lfoInc;
+            if (lfoPhase >= juce::MathConstants<float>::pi * 2.f) {
+                lfoPhase -= juce::MathConstants<float>::pi * 2.f;
+            }
 
-            return y;
+            //update filter coeffs
+            //for (int i = 0; i < 6; i++) {
+            //    allPassDelays[i].Delay(d);
+            //}
+
+            for (auto& e : allPassDelays) {
+                e.Delay(d);
+            }
+
+            //calculate output
+            //float y = allPassDelays[0].Update(
+            //    allPassDelays[1].Update(
+            //        allPassDelays[2].Update(
+            //            allPassDelays[3].Update(
+            //                allPassDelays[4].Update(
+            //                    allPassDelays[5].Update(inSamp + zm1 * feedBack))))));
+
+            // updated for readibility, needs testing see https://godbolt.org/z/P54ve5o51 for live test
+            auto y = std::accumulate(std::next(allPassDelays.rbegin()), allPassDelays.rend(),
+                allPassDelays.back().Update(inSamp + zm1 * feedBack), [](auto acc, auto& e) { return e.Update(acc); });
+            zm1 = y;
+
+            return inSamp + y * depth;
         }
     private:
-        float a1, zm1;
+        class AllPassDelay {
+        public:
+            AllPassDelay()
+                : a1(0.f)
+                , zm1(0.f)
+            {
+            }
+
+            void Delay(float delay) { // sample delay time
+                a1 = (1.f - delay) / (1.f + delay);
+            }
+
+            float Update(float inSamp) {
+                float y = inSamp * -a1 + zm1;
+                zm1 = y * a1 + inSamp;
+
+                return y;
+            }
+        private:
+            float a1, zm1;
+        };
+
+        // AllPassDelay allPassDelays[6];
+        std::array<AllPassDelay, 6> allPassDelays;
+
+        float dmin, dmax; // range
+        float feedBack; 
+        float lfoPhase;
+        float lfoInc;
+        float depth;
+        float sampleRate;
+
+        float zm1;
     };
 
-    // AllPassDelay allPassDelays[6];
-    std::array<AllPassDelay, 6> allPassDelays;
-
-    float dmin, dmax; // range
-    float feedBack; 
-    float lfoPhase;
-    float lfoInc;
-    float depth;
-
-    float zm1;
-};
+} // namespace dsp_jp
